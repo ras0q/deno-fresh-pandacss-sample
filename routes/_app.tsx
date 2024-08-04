53decb4272944ec6cf8323d20f94ee0b05d767f9
ras0q <ras0q@users.noreply.github.com>
2024-08-04 10:06:01 +0900

    add style reference to Panda CSS

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>deno-fresh-pandacss-sample</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/styled-system/styles.css" />
       </head>
       <body>
         <Component />
