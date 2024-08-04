4d79b7daa842b1e399f1aacb4ab245b97ab53a73
ras0q <ras0q@users.noreply.github.com>
2024-08-04 10:06:27 +0900

    rewrite styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,27 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+// @deno-types="../static/styled-system/css/index.d.mts"
+import { css } from "../static/styled-system/css/index.mjs";
 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
+      class={css({
+        px: 2,
+        py: 1,
+        borderColor: "gray.500",
+        borderWidth: 2,
+        borderRadius: "sm",
+        bg: "white",
+        _hover: {
+          bg: "gray.200",
+        },
+        transitionProperty: "background-color, border-color, color, fill, stroke",
+        transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+        transitionDuration: "150ms",
+      })}
     />
   );
 }
