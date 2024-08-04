eb404db42c874f74a0a802e8f0d38fb5189f3a52
ras0q <ras0q@users.noreply.github.com>
2024-08-04 09:16:54 +0900

    change Panda CSS settings for Fresh

--- panda.config.mjs
+++ panda.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ["./{components,islands,routes}/**/*.{js,jsx,ts,tsx}"],
 
   // Files to exclude
   exclude: [],
@@ -16,5 +16,6 @@ export default defineConfig({
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: "./static/styled-system",
+  forceConsistentTypeExtension: true,
 });
